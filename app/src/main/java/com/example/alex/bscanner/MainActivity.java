bf1a645ec5dd6371b870f327c57032d07471033e
Alexander Shcherbachenko <ascherbachenko@simtechdev.com>
2017-07-21 23:58:07 +0300

    button click handler added

--- app/src/main/java/com/example/alex/bscanner/MainActivity.java
+++ app/src/main/java/com/example/alex/bscanner/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        TextView placeholder = (TextView) findViewById(R.id.placeholder);
+
+        if (id == R.id.action_scan) {
+            placeholder.setText("Sucess");
+        } else {
+            placeholder.setText("Fialure");
+        }
+
         return true;
     }
 }
