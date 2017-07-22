8058b3c4f3aee934a5d9ff3e59e3bc3d1001ced1
Alexander Shcherbachenko <ascherbachenko@simtechdev.com>
2017-07-22 16:53:44 +0300

    scannig for other devices logic implemented

--- app/src/main/java/com/example/alex/bscanner/MainActivity.java
+++ app/src/main/java/com/example/alex/bscanner/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.alex.bscanner;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -15,6 +16,10 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "BScanner";
     BluetoothAdapter mBluetoothAdapter;
@@ -22,15 +27,15 @@ public class MainActivity extends AppCompatActivity {
     private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
-            /*if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 // Discovery has found a device. Get the BluetoothDevice
                 // object and its info from the Intent.
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 String deviceName = device.getName();
                 String deviceHardwareAddress = device.getAddress(); // MAC address
-            }*/
-            Log.d(TAG, action);
-            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
+                Log.d(TAG, deviceName);
+                Log.d(TAG, deviceHardwareAddress);
+            } else if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
                 final int state = intent.getIntExtra(mBluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
 
                 if (state == BluetoothAdapter.STATE_ON) {
@@ -68,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
                 registerReceiver(mBroadcastReceiver, BTIntent);
             } else {
-
+                getDevicesList();
             }
 
             /*String[] devicesList = getDevicesList();
@@ -79,9 +84,16 @@ public class MainActivity extends AppCompatActivity {
             ListView listView = (ListView) findViewById(R.id.devices);
             listView.setAdapter(adapter);*/
         } else {
+            getDevicesList();
         }
 
         return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mBroadcastReceiver);
+        super.onDestroy();
     }
 
     protected boolean isBluetoothEnabled() {
@@ -119,7 +131,18 @@ public class MainActivity extends AppCompatActivity {
         dialog.show();
     }*/
 
-    protected String[] getDevicesList() {
+    protected void getDevicesList() {
+        mBluetoothAdapter.startDiscovery();
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mBroadcastReceiver, filter);
+        /*Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        List<String> s = new ArrayList<String>();
+        for(BluetoothDevice bt : pairedDevices)
+            s.add(bt.getName());
+
+        //setListAdapter(new ArrayAdapter<String>(this, R.layout.list, s));
         // implement fetching logic
         String[] devicesList = {"Device 1","Device 2","Device 3","Device 4",
                 "Device 5","Device 6","Device 7","Device 8","Device 9","Device 10","Device 11","Device 12",
@@ -128,6 +151,8 @@ public class MainActivity extends AppCompatActivity {
 
         // implement sorting logic
 
-        return devicesList;
+        Log.d(TAG, s.get(0));
+
+        return devicesList;*/
     }
 }
